5c7b92a246d7f347f420c0c313f1948541c1104e
mfebriann <muhammadfebriyan86@gmail.com>
2024-02-17 17:47:39 +0700

    add screen author, topic, and post

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,8 @@
 import Home from "./Home";
 import Topics from "./Topics";
 import About from "./About";
+import Author from "./Author";
+import Topic from "./Topic";
+import Post from "./Post";
 
-export { Home, Topics, About };
+export { Home, Topics, About, Author, Topic, Post };
