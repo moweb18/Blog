5fefa3d0273a011f1eee8772bb5b08f9ed914629
mfebriann <muhammadfebriyan86@gmail.com>
2024-02-17 17:40:28 +0700

    add function convert date

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,27 @@
 export const URL_API = "https://dashboard.modifwebsite.id/api";
+
+export const convertDate = (date) => {
+  const dateParts = date.split(" ");
+  const datePart = dateParts[0];
+  const dateSplit = datePart.split("-");
+  const year = parseInt(dateSplit[0]);
+  const month = parseInt(dateSplit[1]);
+  const day = parseInt(dateSplit[2]);
+
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  return day + " " + monthNames[month - 1] + " " + year;
+};
