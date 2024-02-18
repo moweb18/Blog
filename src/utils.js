8659fb06e7c2b145ddd56f334695750f2b4df83a
mfebriann <muhammadfebriyan86@gmail.com>
2024-02-18 22:45:30 +0700

    make component and function for share article

--- src/utils.js
+++ src/utils.js
@@ -25,3 +25,48 @@ export const convertDate = (date) => {
 
   return day + " " + monthNames[month - 1] + " " + year;
 };
+
+export const shareArticle = (socialMediaName) => {
+  const text = document
+    .querySelector('meta[property="og:title"]')
+    .getAttribute("content");
+  const image = document
+    .querySelector('meta[property="og:image"]')
+    .getAttribute("content");
+  const currentURL = window.location.href;
+  const message = `${encodeURIComponent(text)}
+			%0A%0A
+			${encodeURIComponent(currentURL)}`;
+
+  if (socialMediaName == "WhatsApp") {
+    window.open(
+      `https://api.whatsapp.com/send?text=${message}&image=${image}`,
+      "_blank",
+      "width=600,height=600",
+    );
+  } else if (socialMediaName == "X") {
+    window.open(
+      `https://twitter.com/intent/tweet?text=${message}&image=${image}`,
+      "_blank",
+      "width=600,height=600",
+    );
+  } else if (socialMediaName == "LinkedIn") {
+    window.open(
+      `https://www.linkedin.com/shareArticle?url=${currentURL}&image=${encodeURIComponent(image)}`,
+      "_blank",
+      "width=600,height=600",
+    );
+  } else if (socialMediaName == "Telegram") {
+    window.open(
+      `https://telegram.me/share/url?url=${message}&image=${image}`,
+      "_blank",
+      "width=600,height=600",
+    );
+  } else if (socialMediaName == "Facebook") {
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${currentURL}&display=popup&quote=${text}&image=${image}`,
+      "_blank",
+      "width=600,height=600",
+    );
+  }
+};
